d1e8732f340d0f51292a9493a45b894f1da95dbe
olivier <olibitter@gmail.com>
2023-12-10 14:58:40 +0100

    day-10: Solve part 1

--- day-10/part1.py
+++ day-10/part1.py
@@ -1,6 +1,4 @@
-import operator
 import numpy as np
-from numpy.core.multiarray import array
 
 
 BELOW = np.array([1, 0])
@@ -24,35 +22,43 @@ def read_input(filename):
         lines = [line.rstrip() for line in file]
     return lines
 
-
-def sub(p1, p2):
-    return tuple(x1 - x2 for x1, x2 in zip(p1, p2))
-
-
-def add(p1, p2):
-    return tuple(x1 + x2 for x1, x2 in zip(p1, p2))
-
-
 def move(current_pos, prev_pos, pipe):
     direction = prev_pos - current_pos
-    next_direction = (pipes[pipe][all(pipes[pipe][0] == direction)])
+    next_direction = pipes[pipe][all(pipes[pipe][0] == direction)]
     next_pos = current_pos + next_direction
-
     return next_pos, next_direction * -1
 
 
-def solve(lines):
-    pass
+def get_first_position(start_pos, pipe_map):
+    if pipe_map[tuple(start_pos + RIGHT)] in ["-", "J", "7"]:
+        return start_pos + RIGHT
+    if pipe_map[tuple(start_pos + LEFT)] in ["-", "F", "L"]:
+        return start_pos + LEFT
+    if pipe_map[tuple(start_pos + ABOVE)] in ["|", "F", "7"]:
+        return start_pos + ABOVE
+    else:
+        return start_pos + BELOW
 
 
 def main():
-    lines = read_input("sample1.txt")
-    # lines = read_input("input.txt")
-    for line in lines:
-        print(line)
-    solve(lines)
+    # lines = read_input("sample1.txt")
+    # lines = read_input("sample2.txt")
+    lines = read_input("input.txt")
+    pipe_map = np.array(list(map(list, lines)))
+    start_pos = np.argwhere(pipe_map == "S")[0]
+
+    prev_pos = start_pos
+    current_pos = get_first_position(start_pos, pipe_map)
+    dist = 1
+
+    while not np.array_equal(current_pos, start_pos):
+        next_pos, _ = move(current_pos, prev_pos, pipe_map[tuple(current_pos)])
+        prev_pos = current_pos
+        current_pos = next_pos
+        dist += 1
+
+    print(dist // 2)
 
 
 if __name__ == "__main__":
-    print(sub((1, 2), (3, 4)))
     main()
