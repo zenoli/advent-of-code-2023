b7e0c49f08de9bed2b796db134bcd504a88c104c
olivier <olibitter@gmail.com>
2023-12-11 18:43:58 +0100

    day-11: Solve part 1

--- day-11/part1.py
+++ day-11/part1.py
@@ -1,4 +1,5 @@
 import numpy as np
+from itertools import combinations
 
 
 def read_input(filename):
@@ -11,6 +12,7 @@ def debug(universe):
     universe_pretty = np.empty_like(universe)
     universe_pretty = np.full(universe.shape, " ")
     universe_pretty[universe] = "#"
+    universe_pretty[universe == False] = "."
     print(universe_pretty)
 
 
@@ -25,14 +27,25 @@ def expand(universe_raw):
     return np.insert(universe, empty_rows(universe_raw), False, axis=0)
 
 
+def galaxies(universe):
+    return zip(*np.where(universe))
+
+
+def manhattan_distance(a, b):
+    return sum(abs(x1 - x2) for x1, x2 in zip(a, b))
+
+
 def main():
-    lines = read_input("sample.txt")
-    # lines = read_input("input.txt")
+    # lines = read_input("sample.txt")
+    lines = read_input("input.txt")
 
     universe_raw = np.array(list(map(list, lines))) == "#"
     universe = expand(universe_raw)
-    debug(universe_raw)
-    debug(universe)
+
+    result = sum(
+        manhattan_distance(g1, g2) for g1, g2 in combinations(galaxies(universe), 2)
+    )
+    print(result)
 
 
 if __name__ == "__main__":
