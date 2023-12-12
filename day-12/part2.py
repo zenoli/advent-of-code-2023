cbfceca075c692d63b026e5f7294d8f5879d7598
olivier <olibitter@gmail.com>
2023-12-12 19:12:33 +0100

    day-12: Part2: setup

---
+++ day-12/part2.py
@@ -0,0 +1,49 @@
+def read_input(filename):
+    with open(filename) as file:
+        lines = [line.rstrip() for line in file]
+    return lines
+
+
+def f(X, c):
+    if not c:
+        return 0 if "#" in X else 1
+    if not X:
+        return 0
+
+    if X[0] == ".":
+        return f(X[1:], c)
+
+    if X[0] == "#":
+        if can_place(X, c[0]):
+            return f(X[c[0] + 1 :], c[1:])
+        else:
+            return 0
+
+    return f("#" + X[1:], c) + f("." + X[1:], c)
+
+
+def can_place(X, group):
+    if len(X) < group:
+        return False
+    if len(X) > group and X[group] == "#":
+        return False
+    return set(X[:group]) <= {"#", "?"}
+
+
+def parse_line(line):
+    record, groups = line.split()
+    groups = list(map(int, ((groups + ",") * 5)[:-1].split(",")))
+    return "?".join([record] * 5), groups
+
+
+def main():
+    lines = read_input("sample.txt")
+    lines = read_input("input.txt")
+    result = sum(f(record, group) for record, group in map(parse_line, lines[:1]))
+    print(result)
+    for record, group in map(parse_line, lines):
+        print(record, group)
+
+
+if __name__ == "__main__":
+    main()
