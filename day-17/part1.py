41dfba76ec5d24f3b66da188d85c4de2e91cf79b
olivier <olibitter@gmail.com>
2023-12-21 22:35:15 +0100

    day-17: Solve part 1 (slow)

--- day-17/part1.py
+++ day-17/part1.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Mapping, Tuple
 import numpy as np
 import math
 
@@ -10,7 +10,7 @@ DOWN: Direction = (1, 0)
 LEFT: Direction = (0, -1)
 RIGHT: Direction = (0, 1)
 
-directions: list[Direction] = [UP, DOWN, LEFT, RIGHT]
+directions: Mapping[Direction, int] = {UP: 0, DOWN: 1, LEFT: 2, RIGHT: 3}
 
 arrow_symbols = {UP: "", DOWN: "", LEFT: "", RIGHT: ""}
 
@@ -23,6 +23,16 @@ def sub(a: tuple[int, int], b: tuple[int, int]) -> tuple[int, int]:
     return a[0] - b[0], a[1] - b[1]
 
 
+def rot_right(direction):
+    x, y = direction
+    return y, -x
+
+
+def rot_left(direction):
+    x, y = direction
+    return -y, x
+
+
 def read_input(filename):
     with open(filename) as file:
         lines = [list(map(int, line.strip())) for line in file]
@@ -33,38 +43,58 @@ def on_grid(pos, N, M):
     return pos[0] in range(N) and pos[1] in range(M)
 
 
-def main():
-    # lines = read_input("input.txt")
-    grid = read_input("sample.txt")
-    distances = np.full(grid.shape, math.inf)
-    visited = np.full(grid.shape, False)
+def get_neighbors(pos, d, c, N, M):
+    neighbors = [(add(pos, new_d), new_d, 1) for new_d in [rot_right(d), rot_left(d)]]
+    if c + 1 <= 3:
+        neighbors.append((add(pos, d), d, c + 1))
+    return [n for n in neighbors if on_grid(n[0], N, M)]
 
-    distances[0, 0] = 0
+
+def pop_next(queue, distances):
+    min_index = -1
+    min_dist = math.inf
+    for i, (pos, d, c) in enumerate(queue):
+        pos_index = (*pos, directions[d], c)
+        if distances[pos_index] < min_dist:
+            min_index = i
+            min_dist = distances[pos_index]
+    return queue.pop(min_index)
+
+
+def main():
+    # grid = read_input("input.txt")
+    # grid = read_input("sample.txt")[:6,:12]
+    grid = read_input("sample.txt")
+    # grid = read_input("sample2.txt")
+    distances = np.full((*grid.shape, 4, 4), np.iinfo(np.int32).max)
+    visited = np.full((*grid.shape, 4, 4), False)
+
+    distances[0, 0, :, :] = 0
 
     N, M = grid.shape
-    arrows = np.array([set() for _ in range(N * M)]).reshape(N, M)
-    arrows[0, 0].add(UP)
 
-    queue: list[Position] = [(0, 0)]
+    queue: list[tuple[Position, Direction, int]] = [
+        ((0, 0), RIGHT, 0),
+        ((0, 0), DOWN, 0),
+    ]
 
-    while not visited[-1, -1]:
-        pos = queue.pop(0)
-        if visited[pos]:
+    while queue:
+        # pos, d, c = queue.pop(0)
+        pos, d, c = pop_next(queue, distances)
+
+        pos_index = (*pos, directions[d], c)
+        if visited[pos_index]:
             continue
-        visited[pos] = True
+        visited[pos_index] = True
 
-        for direction in directions:
-            neighbor = add(pos, direction)
-            if not on_grid(neighbor, N, M):
-                continue
-            if not visited[neighbor]:
-                queue.append(neighbor)
-            new_dist = distances[pos] + grid[neighbor]
+        for new_pos, new_d, new_c in get_neighbors(pos, d, c, N, M):
+            new_pos_index = (*new_pos, directions[new_d], new_c)
+            if not visited[new_pos_index]:
+                queue.append((new_pos, new_d, new_c))
+            new_dist = distances[pos_index] + grid[new_pos]
+            distances[new_pos_index] = min(distances[new_pos_index], new_dist)
 
-            distances[neighbor] = min(distances[neighbor], new_dist)
-
-    print(distances)
-
+    print(distances[-1, -1, :, 1:].min())
 
 
 if __name__ == "__main__":
---
+++ day-17/test_part1.py
@@ -0,0 +1,41 @@
+from part1 import get_neighbors, UP, DOWN, LEFT, RIGHT, add, sub, rot_left, rot_right
+
+
+def test_get_neighbors():
+    neighbors = get_neighbors((1, 1), RIGHT, 2, 3, 3)
+    assert set(neighbors) == {
+        ((0, 1), UP, 1),
+        ((2, 1), DOWN, 1),
+        ((1, 2), RIGHT, 3),
+    }
+
+    neighbors = get_neighbors((1, 1), RIGHT, 3, 3, 3)
+    assert set(neighbors) == {((0, 1), UP, 1), ((2, 1), DOWN, 1)}
+
+    neighbors = get_neighbors((1, 1), RIGHT, 2, 2, 2)
+    assert set(neighbors) == {((0, 1), UP, 1)}
+
+    neighbors = get_neighbors((1, 1), DOWN, 1, 3, 3)
+    assert set(neighbors) == {((1, 0), LEFT, 1), ((1, 2), RIGHT, 1), ((2, 1), DOWN, 2)}
+
+
+def test_add():
+    assert add((1, 2), (2, 3)) == (2, 5)
+
+
+def test_sub():
+    assert sub((3, 2), (1, 2)) == (2, 0)
+
+
+def test_rot_right():
+    assert rot_right(UP) == RIGHT
+    assert rot_right(RIGHT) == DOWN
+    assert rot_right(DOWN) == LEFT
+    assert rot_right(LEFT) == UP
+
+
+def test_rot_left():
+    assert rot_left(RIGHT) == UP
+    assert rot_left(DOWN) == RIGHT
+    assert rot_left(LEFT) == DOWN
+    assert rot_left(UP) == LEFT
