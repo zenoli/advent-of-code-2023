183788eda422b4af6a98172dae076bc0aebf8f49
olivier <olibitter@gmail.com>
2023-12-23 18:27:23 +0100

    day-18: Solve part 1

---
+++ day-18/part1.py
@@ -0,0 +1,82 @@
+from typing import Mapping
+from functools import reduce
+from itertools import pairwise
+import numpy as np
+from skimage.morphology import flood
+
+
+Position = tuple[int, int]
+Direction = Position
+
+UP: Direction = (-1, 0)
+DOWN: Direction = (1, 0)
+LEFT: Direction = (0, -1)
+RIGHT: Direction = (0, 1)
+
+directions: Mapping[str, Direction] = {"U": UP, "D": DOWN, "L": LEFT, "R": RIGHT}
+
+
+def add(a: tuple[int, ...], b: tuple[int, ...]) -> tuple[int, ...]:
+    return tuple(map(sum, zip(a, b)))
+
+
+def mul(a: tuple[int, ...], factor: int) -> tuple[int, ...]:
+    return tuple(map(lambda x: x * factor, a))
+
+
+def sub(a: tuple[int, ...], b: tuple[int, ...]) -> tuple[int, ...]:
+    return add(a, mul(b, -1))
+
+
+def read_input(filename):
+    def parse(line):
+        dir, steps, color = line.split()
+        return directions[dir], int(steps), color[1:-1]
+
+    with open(filename) as file:
+        return list(map(parse, file))
+
+
+def get_coords(instructions):
+    def coord_reducer(coords, instruction):
+        dir, steps, _ = instruction
+        return coords + [add(coords[-1], mul(dir, steps))]
+
+    return reduce(coord_reducer, instructions, [(0, 0)])
+
+
+def shift_coords(coords):
+    res = np.array(coords)
+    return res - np.min(res, axis=0)
+
+
+def draw(coords):
+    grid = np.full(np.max(coords, axis=0) + 1, 0)
+    for (x1, y1), (x2, y2) in pairwise(coords):
+        grid[min(x1, x2) : max(x1, x2) + 1, min(y1, y2) : max(y1, y2) + 1] = 1
+    return grid
+
+
+def find_interior_point(grid):
+    return 1, np.nonzero(grid[1])[0][0] + 1
+
+
+def fill_pit(grid):
+    interior_point = find_interior_point(grid)
+    mask = flood(grid, interior_point)
+    grid[mask] = 2
+
+
+def main():
+    # instructions = read_input("sample.txt")
+    instructions = read_input("input.txt")
+
+    coords = get_coords(instructions)
+    coords = shift_coords(coords)
+    grid = draw(coords)
+    fill_pit(grid)
+    print(np.count_nonzero(grid))
+
+
+if __name__ == "__main__":
+    main()
---
+++ day-18/test_part1.py
@@ -0,0 +1,13 @@
+from part1 import DOWN, LEFT, RIGHT, add, get_coords, sub, mul
+
+
+def test_tuple_arithmetic():
+    assert add((0, 1), (1, 0)) == (1, 1)
+    assert sub((1, 1), (1, 0)) == (0, 1)
+    assert mul((1, -2), -1) == (-1, 2)
+
+
+def test_get_coords():
+    instructions = [(DOWN, 6, ""), (RIGHT, 3, "")]
+    coords = get_coords(instructions)
+    assert coords == [(0, 0), (6, 0), (6, 3)]
